74dc1a0bc52b4882a4393447ff7d1778e141a7c7
avnergomes <avnerpaesgomes@gmail.com>
2025-11-07 10:40:10 -0300

    deply

---
+++ client/build/assets/charts-C1nMRUON.js
@@ -0,0 +1,2 @@
+import"./mui-iWwMWcLP.js";import"./vendor-nf7bT_Uh.js";
+//# sourceMappingURL=charts-C1nMRUON.js.map
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🚀 SpaceTraders Control Center</h1>
+      <p>Interface frontend funcionando corretamente!</p>
+    </div>
+  )
+}
+
+export default App
