e324bf954c1780b4a6a704ad8bc3bfc53603597b
avnergomes <avnerpaesgomes@gmail.com>
2025-11-07 10:49:13 -0300

    Updates

--- assets/three-pm-gbMrv.js
+++
@@ -1,2 +0,0 @@
-import{a as r}from"./vendor-nf7bT_Uh.js";import"./mui-iWwMWcLP.js";var o={},t=r;o.createRoot=t.createRoot,o.hydrateRoot=t.hydrateRoot;export{o as c};
-//# sourceMappingURL=three-pm-gbMrv.js.map
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/spacetraders-v1/sw.js', { scope: '/spacetraders-v1/' })})}
